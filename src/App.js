c7044b95731c81d7aef4f5edbd7fe595804b18b2
MrAbuSayed <124281002+MrAbuSayed@users.noreply.github.com>
2023-02-21 17:47:03 +0600

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import logo from './logo.svg';
+import './App.css';
+import Header from './All-Components/Header/Header';
+import Shop from './All-Components/Shop/Shop';
+
+
+function App() {
+  return (
+    <div className="App">
+     
+
+           <Header> </Header>
+           <Shop></Shop>
+
+
+
+    </div>
+  );
+}
+
+export default App;
