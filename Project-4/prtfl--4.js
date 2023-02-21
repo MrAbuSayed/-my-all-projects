013d6202f645dacb1d72d187b3080f37cb554611
MrAbuSayed <124281002+MrAbuSayed@users.noreply.github.com>
2023-02-21 17:39:07 +0600

    Add files via upload

---
+++ Project-4/prtfl--4.js
@@ -0,0 +1,22 @@
+ 
+
+//  MAke Animate 
+
+  $("#About").mouseover(function(){
+    $(".part1").show(1000);
+        $(".part1").fadeTo(2000,1)
+    });
+
+     
+
+    var type = new Typed('.bnrh',{
+        strings:[ "Hello!","Hello!"],
+        typeSpeed:500,
+       
+         
+    });
+    var type = new Typed('.bnrh1',{
+        strings:[ "Iam Abu Sayed","Iam Abu Sayed..."],
+        typeSpeed:100,
+            
+    });
